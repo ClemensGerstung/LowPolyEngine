a587a1d9377dc405904992b73c5a672603010944
Clemens Gerstung <clemens8496@gmail.com>
2017-09-30 22:46:29 +0200

    test

--- LowPolyEngine.Test/Main.cpp
+++ LowPolyEngine.Test/Main.cpp
@@ -1,27 +1,8 @@
 #include <iostream>
-#include "lpe.h"
-#include "Window.h"
-#include "Model.h"
 
 int main()
 {
-  lpe::Window window;
-	try
-	{
-		window.Create(800, 600, "Hello Vulkan", false);
-	  lpe::Model m = window.AddModel("models/tree.ply");
 
-		while(window.IsOpen())
-		{
-			window.Render();
-		}
-	}
-	catch (std::runtime_error e)
-	{
-		std::cerr << e.what() << std::endl;
-
-		return EXIT_FAILURE;
-	}
 
 	return 0;
 }
