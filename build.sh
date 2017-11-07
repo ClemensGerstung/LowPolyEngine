8a2b2d82a36453d6766ed7072e250c6d9dfc4ccd
Fritz Schmid <opzrots@googlemail.com>
2017-11-08 00:02:45 +0100

    ..

--- build.sh
+++ build.sh
@@ -6,16 +6,3 @@ mkdir linux
 cd linux
 cmake ../../../ -DCMAKE_BUILD_TYPE=Release
 make -j $(getconf _NPROCESSORS_ONLN)
-cd ..
-mkdir win
-cd win
-mkdir x86
-cd x86
-cmake ../../../ -DCMAKE_TOOLCHAIN_FILE=../../../external/glfw/CMake/x86_64-w64-mingw32.cmake -DCMAKE_BUILD_TYPE=Release
-make -j $(getconf _NPROCESSORS_ONLN)
-cd ..
-mkdir x64
-cd x64
-cmake ../../../ -DCMAKE_TOOLCHAIN_FILE=../../../external/glfw/CMake/amd64-mingw32msvc.cmake -DCMAKE_BUILD_TYPE=Release
-make -j $(getconf _NPROCESSORS_ONLN)
-cd ..
