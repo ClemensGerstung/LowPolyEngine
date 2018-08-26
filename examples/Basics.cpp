73c9735a66fe267f8e0d9546cff9036ef4654cd2
Clemens Gerstung <clemens8496@gmail.com>
2018-08-26 22:37:26 +0200

    reset (again)  ¯\_(ツ)_/¯

--- examples/Basics.cpp
+++ examples/Basics.cpp
@@ -1,34 +1,7 @@
 #include <iostream>
-#include "../src/lpe.h"
 
 int main()
 {
-  auto id = typeid(lpe::window::Window).raw_name();
-
-  lpe::window::Window window("Hello World",
-                             1920,
-                             1080,
-                             true);
-
-  window.KeyPressed += [](const lpe::window::Window& window,
-                          const lpe::window::WindowKeyPressedEventArgs& e)
-  {
-    std::cout << static_cast<uint32_t>(e.GetKey()) << std::endl;
-  };
-
-  //lpe::vulkan::VulkanManager::EnableValidationLayers = false;
-
-  lpe::Initialize();
-
-  lpe::ServiceLocator& locator = lpe::ServiceLocator::Default;
-  auto vulkan = locator.Renderer;
-
-  while (window.IsOpen())
-  {
-    window.Render();
-  }
-
-  lpe::Close();
 
   return 0;
 }
